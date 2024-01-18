f1b3e21241550a37aca9e13146ae52bf13d72ec0
Nicola <ncl.rss.92@gmail.com>
2024-01-18 09:22:49 +0100

    First commit

---
+++ omega-monolith/src/main/java/it/aleph/omegamonolith/OmegaMonolithApplication.java
@@ -0,0 +1,13 @@
+package it.aleph.omegamonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmegaMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmegaMonolithApplication.class, args);
+	}
+
+}
---
+++ omega-monolith/src/test/java/it/aleph/omegamonolith/OmegaMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aleph.omegamonolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmegaMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
