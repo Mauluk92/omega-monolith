4541cc343e561fa96f930cf460f1bd4865760780
Nicola <ncl.rss.92@gmail.com>
2024-01-18 11:52:32 +0100

    Create entity repository for catalog subsection

---
+++ omega-monolith/src/main/java/it/aleph/omegamonolith/dao/catalog/BookRepository.java
@@ -0,0 +1,9 @@
+package it.aleph.omegamonolith.dao.catalog;
+
+import it.aleph.omegamonolith.model.catalog.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
